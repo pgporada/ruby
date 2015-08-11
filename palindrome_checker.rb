be75bea4fb1699f042912cbe0beff77d6230682e
Phil <philporada@gmail.com>
2015-08-11 00:08:05 -0400

    Rewrote my palindrome checker in Ruby

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -2,21 +2,34 @@
 # WHO:   PGP
 # WHAT:  This script will check if the string you input is a palindrome
 
-# Take the first argument as the input
-arg = ARGV[0]
-chars = arg.split(//)
-
-# Get scalar length of the array
-size = chars.length
-
-# Divide the number and round up to find where our search should stop
-half = (size/2.to_f).ceil
-
-for i in 0..half
-    if chars[i].downcase != chars[-i-1].downcase
-        puts "Your string ==> '#{arg}' <== is NOT a palindrome :("
-        exit
-    end
+def usage
+    puts "Usage:"
+    puts "Enter a string as your first and only argument"
+    exit
 end
 
-puts "Your string ==> '#{arg}' <== is a palindrome! :)"
+def check_palindrome(uarg)
+    chars = uarg.split(//)
+
+    # Get scalar length of the array
+    size = chars.length
+
+    # Divide the number and round up to find where our search should stop
+    half = (size/2.to_f).ceil
+
+    for i in 0..half
+        if chars[i].downcase != chars[-i-1].downcase
+            puts "Your string ==> '#{uarg}' <== is NOT a palindrome :("
+            exit
+        end
+    end
+    puts "Your string ==> '#{uarg}' <== is a palindrome! :)"
+end
+
+if ARGV.empty?
+    usage
+    exit
+end
+
+arg = ARGV[0]
+check_palindrome(arg)
