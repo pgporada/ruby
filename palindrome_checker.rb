2061e72162b955385b535ae7ba3e10099c625ae3
Phil <philporada@gmail.com>
2015-08-11 00:51:33 -0400

    Added support for palindromic sentences

--- palindrome_checker.rb
+++ palindrome_checker.rb
@@ -5,20 +5,31 @@
 def usage
     puts "Usage:"
     puts "Enter a string as your first and only argument"
+    puts "Example:"
+    puts "#{$0} \"No 'x' in Nixon\""
     exit
 end
 
 def check_palindrome(uarg)
-    chars = uarg.split(//)
+    chars = uarg.delete(' ').split(//)
+
+    # Strip non-alphanumeric chars as we want to be able to handle a palindromic sentence 
+    chars.each do |x|
+        chars = x.scan(/\w/)
+    end
 
     # Get scalar length of the array
     size = chars.length
+    if size == 1
+        puts "Your string ==> '#{uarg}' <== is a palindrome! :)"
+        exit
+    end
 
     # Divide the number and round up to find where our search should stop
     half = (size/2.to_f).ceil
 
     for i in 0..half
-        if chars[i].downcase != chars[-i-1].downcase
+        if chars[i].to_s.downcase != chars[-i-1].to_s.downcase
             puts "Your string ==> '#{uarg}' <== is NOT a palindrome :("
             exit
         end
